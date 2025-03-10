b7cde3d4c9e0b87d2c0c85d30817e76824b0f171
TinFoiKa <117747442+TinFoiKa@users.noreply.github.com>
2025-03-10 12:05:22 +0800

    does the build folder follow?

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -9,6 +9,7 @@ const Map: React.FC<MapProps> = () => {
   return (
     <div className="map-container">
       {/* Map implementation will go here */}
+      Basic Tester
       {/* You can integrate libraries like Leaflet, Mapbox, or Google Maps here */}
     </div>
   );
